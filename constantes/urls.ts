0111170c2da38472068b016a516112cbff912a60
joseent <jose.ent17@gmail.com>
2023-09-15 22:46:53 -0300

    vercel adrees

--- constantes/urls.ts
+++ constantes/urls.ts
@@ -1,2 +1,2 @@
-export const URL_DOMAIN = "https://ctd-esp-fe3-final-r8dcnhvf5-joseent.vercel.app/"
+export const URL_DOMAIN = "https://ctd-esp-fe3-final-nt.vercel.app"
 // export const URL_DOMAIN = "http://localhost:3000/"
